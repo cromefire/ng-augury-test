ae19176b7182ff58939f2c8eaeff59af9dc2b9ac
cromefire <tim.langhorst@outlook.de>
2019-09-03 15:04:13 +0200

    added enableDebugTools()

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
-import { enableProdMode } from '@angular/core';
+import {ApplicationRef, enableProdMode} from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {enableDebugTools} from "@angular/platform-browser";
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+async function bootstrap() {
+  const module = await platformBrowserDynamic().bootstrapModule(
+      AppModule
+  );
+  if (!environment.production) {
+    const applicationRef = module.injector.get(ApplicationRef);
+    const appComponent = applicationRef.components[0];
+    enableDebugTools(appComponent);
+  }
+  return module;
+}
+
+bootstrap().catch(err => console.error(err));
+
